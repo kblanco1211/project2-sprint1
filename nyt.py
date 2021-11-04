2bcd3004bbdec7c71d52fcdce7fa758dcdcd5131
kblanco1211 <kblanco2@student.gsu.edu>
2021-11-04 15:01:28 -0400

    implemented nyt's most popular api updated commit to not include api key

--- nyt.py
+++ nyt.py
@@ -1,1 +1,12 @@
-# implement all nyt api logic here
+import requests
+import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+API_KEY = os.getenv("API_KEY")
+response = requests.get(
+    f"https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key={API_KEY}"
+)
+
+print(response.json())
