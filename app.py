f191e7f4c8e8cce0e091e8821630ad579169cb5d
kblanco1211 <kblanco2@student.gsu.edu>
2021-11-04 13:39:57 -0400

    added boiler plate and base routes for app

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-# implement main app/flask logic here
+import flask
+import os
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/saved")
+def saved():
+    return flask.render_template("saved.html")
+
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return flask.render_template("signup.html")
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
