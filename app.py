788a3b60fda12a18929d6aef9823f3e1cda66165
kblanco1211 <kblanco2@student.gsu.edu>
2021-11-08 11:51:50 -0500

    added everything needed to deploy and create database

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import flask
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
