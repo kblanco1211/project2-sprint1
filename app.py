083fcae7c6899bcb490250442e9b43fa421cdf5c
Romina <rominakarimaee@yahoo.com>
2021-11-14 02:05:25 +0000

    added my parts

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 # implement main app/flask logic here
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def signup():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def login():
+    return render_template('signup.html')
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "127.0.0.1"),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True,
+    )
