9663751ee9f273a7ae889ee69e7b909785989cee
Romina <rominakarimaee@yahoo.com>
2021-11-15 14:58:54 +0000

    Removed the forgot pass

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def signup():
     return flask.render_template("signup.html")
 
 
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
+app.run(host=os.getenv("IP", "127.0.0.1"), port=int(os.getenv("PORT", 8080)), debug=True)
