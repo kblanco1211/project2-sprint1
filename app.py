7e4d418461dda503debc766f046770f6499440e4
nalbano1 <nalbano1@student.gsu.edu>
2021-11-16 14:05:14 +0000

    secret update

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from models import UserModel,db,login
 load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
-
+app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite://data.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 db.init_app(app)
 login.init_app(app)
