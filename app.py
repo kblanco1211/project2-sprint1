0255d0ee0b287488449bc0f09e3f26a06f8a0c61
Romina <rominakarimaee@yahoo.com>
2021-11-16 02:33:27 +0000

    Fixed some errors

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import flask
 import os
+from flask_login import login_manager
 from flask_sqlalchemy import SQLAlchemy
+from flask_login.utils import login_required
+
 
 app = flask.Flask(__name__)
 
@@ -8,6 +11,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+# db.create_all()
+# login_manager = LoginManager()
+# login_manager.login_view = "login"
+# login_manager.init_app(app)
+
+# @login_manager.user_loader
+# def load_user(user_name):
+#     return User.query.get(user_name)
 
 
 @app.route("/")
@@ -30,4 +42,4 @@ def signup():
     return flask.render_template("signup.html")
 
 
-app.run(host=os.getenv("IP", "127.0.0.1"), port=int(os.getenv("PORT", 8080)), debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
