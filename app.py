78bad8706112553c7a9480a4c0a75d1ddebdc8b2
nalbano1 <nalbano1@student.gsu.edu>
2021-11-16 20:27:45 +0000

    working lcoal

--- app.py
+++ app.py
@@ -188,5 +188,5 @@ def future():
 
 
 app.run(
-    host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8080")), debug=True
+    host="localhost", port=int(os.getenv("PORT", "8080")), debug=True
 )
